a0210ac6c335e719cb8f700bd56bec486475ab18
jayndlicia <heslicia@UMICH.EDU>
2016-06-03 17:35:21 -0400

    http://answers.unity3d.com/questions/549850/saving-informations-from-game.html

---
+++ ivf-unity-master/Assets/Scripts/SurveyManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class SurveyManager : MonoBehaviour {
+
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text questionText;
+
+    [SerializeField]
+    private float timeBetweenQuestions = 1f;
+	// Use this for initialization
+	void Start () {
+        if(unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+
+        SetCurrentQuestion();
+        Debug.Log(currentQuestion.question + " is " + currentQuestion.isTrue);
+	}
+	
+    void SetCurrentQuestion()
+    {
+        int QuestionIndex = 0;
+        currentQuestion = unansweredQuestions[QuestionIndex];
+
+        questionText.text = currentQuestion.question;
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.RemoveAt(currentQuestion);
+        yield return new WaitForSeconds(timeBetweenQuestions);
+
+    }
+
+    public void UserSelectTrue()
+    {
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("You selected True. That's positive");
+        }
+        else
+        {
+            Debug.Log("False. That's negative");
+        }
+    }
+
+    public void UserSelectFalse()
+    {
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("You selected False. That's positive");
+        }
+        else
+        {
+            Debug.Log("True. That's negative");
+        }
+    }
+}
